58f2de75fb7361e09a8e8acd33db93982a2b8ab5
nekosnugz <orlando.rossi2609@gmail.com>
2025-02-13 20:18:45 +0100

    Finished source, edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# NewRepo
+# Hello World!
+<p>This is a generic Hello World program I made to mark my beginning in C++ programming</p>
--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
-    cin;
+    //Simple C++ Hello World
+    cout << "Hello C++ World!\n";
+
+    //Cursed(?) C++ Hello World
+    printf("Hello C++ World, in C style!\n");
     return 0;
 }
